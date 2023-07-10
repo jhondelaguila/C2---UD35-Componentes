e11f1846caf6da51efd69d1c6d1f6a4ae185da48
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-07-10 19:28:16 +0200

    wip problema 4

--- problema4/src/app/app-routing.module.ts
+++ problema4/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormulariotareaComponent } from './formulariotarea/formulariotarea.component';
+import { ListatareasComponent } from './listatareas/listatareas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'formulario',
+    component: FormulariotareaComponent
+  },
+  {
+    path: 'lista',
+    component: ListatareasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ problema4/src/app/formulariotarea/formulariotarea.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulariotarea',
+  templateUrl: './formulariotarea.component.html',
+  styleUrls: ['./formulariotarea.component.css']
+})
+export class FormulariotareaComponent {
+  
+}
