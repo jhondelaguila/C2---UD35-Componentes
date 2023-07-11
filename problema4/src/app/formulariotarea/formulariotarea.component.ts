19906c11e2d0590437af9c0a3f8daca1f79f230d
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-07-11 11:29:50 +0200

    problema4 acabado

--- problema4/src/app/formulariotarea/formulariotarea.component.ts
+++ problema4/src/app/formulariotarea/formulariotarea.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./formulariotarea.component.css']
 })
 export class FormulariotareaComponent {
-  
+  titulo:string = '';
+  descripcion:string = '';
+  tareas:any = [];
+  id:number = 0;
+
+  agregar(){
+    this.tareas.push(
+      {
+        id: this.id++,
+        titulo: this.titulo,
+        descripcion: this.descripcion,
+        completado: true
+      }
+    );
+
+    this.titulo = '';
+    this.descripcion = '';
+  }
+
+  updateList(updatedList:any){
+    this.tareas = updatedList;
+  }
 }
--- problema4/src/app/listatareas/listatareas.component.ts
+++ problema4/src/app/listatareas/listatareas.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit,EventEmitter,Output } from '@angular/core';
 
 @Component({
   selector: 'app-listatareas',
   templateUrl: './listatareas.component.html',
   styleUrls: ['./listatareas.component.css']
 })
-export class ListatareasComponent {
+export class ListatareasComponent implements OnInit{
+  @Input() listaTareas:any[] = [];
+  @Output() updatedList = new EventEmitter();
 
+  constructor(){}
+
+  ngOnInit(): void {
+  }
+
+  eliminarTarea(id_tarea:number){
+    let newLista = this.listaTareas.filter(tarea => tarea.id !== id_tarea);
+    this.listaTareas = newLista;
+    this.updatedList.emit(this.listaTareas);
+  }
+
+  completado(completado:boolean):boolean{
+    return !completado;
+  }
 }
